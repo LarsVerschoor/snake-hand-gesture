7202923e0d4b1d0da7d396e0c85de745337cca2d
LarsVerschoor <larsverschoor2@gmail.com>
2025-06-15 17:17:10 +0200

    Added training

--- training-model/src/js/script.js
+++ training-model/src/js/script.js
@@ -1,13 +1,46 @@
 ml5.setBackend("webgl");
 const nn = ml5.neuralNetwork({ task: 'classification', debug: true });
 
-nn.addData([18,9.2,8.1,2], {label:"cat"})
-nn.addData([20.1,17,15.5,5], {label:"dog"})
+const trainButton = document.getElementById('train');
+const input = document.getElementById('data');
 
+let trainingData = null;
+let testingData = null;
 
-nn.normalizeData()
-nn.train({ epochs: 10 }, () => finishedTraining())
+input.addEventListener('change', async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const text = await file.text();
+    const data = JSON.parse(text);
+
+    [trainingData, testingData] = (() => {
+        const index = Math.floor(data.length * 0.2);
+        return [data.slice(index), data.slice(0, index)];
+    })();
+
+    console.log(testingData.length, trainingData.length)
+});
+
+trainButton.addEventListener('click', () => {
+    if (!trainingData || !testingData) return;
+
+    trainingData.forEach((item) => {
+        nn.addData(item.points, {label: item.label});
+    });
+
+    nn.normalizeData()
+
+    nn.train({ epochs: 100 }, () => finishedTraining())
+});
+
 async function finishedTraining(){
-    const results = await nn.classify([29,11,10,3])
-    console.log(results)
+    nn.save("model", () => console.log("model was saved!"));
+    const blob = new Blob([JSON.stringify(testingData)], {type: 'application/json'});
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'testing-data.json';
+    a.click();
+    URL.revokeObjectURL(url);
 }
